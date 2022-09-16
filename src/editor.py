fedce01b958c73beeefae6d884874229666b4fcf
Konstantin <zaikinke@yandex.ru>
2022-09-16 18:05:05 +0500

    Basic architecture

---
+++ src/editor.py
@@ -0,0 +1,21 @@
+import curses
+
+
+class Window:
+    pass
+
+
+class Cursor:
+    pass
+
+
+class Buffer:
+    pass
+
+
+def main(stdscr):
+    pass
+
+
+if __name__ == "__main__":
+    curses.wrapper(main)
