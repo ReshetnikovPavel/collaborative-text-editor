e12034317f3b0107a6f861f53bb2640a0e6c7a3f
Konstantin <zaikinke@yandex.ru>
2022-09-20 00:47:47 +0500

    Refactor parse_args

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,7 @@
-from typing import List
 import argparse
 
 
-def parse_args(arguments: List[str]) -> argparse.Namespace:
+def parse_args(*arguments: str) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     for arg in arguments:
         parser.add_argument(arg)
